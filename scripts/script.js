6ade802522995b8ec41c15a4ca19a678be8df486
unknown <ae-m7@mail.ru>
2023-01-24 22:39:39 +0300

    overlay and accrodion

---
+++ scripts/script.js
@@ -0,0 +1,84 @@
+const modalController= ({modal, btnOpen, btnClose}) => {
+  const buttonElem = document.querySelector(btnOpen);
+  const modalElem = document.querySelector(modal);
+  //TODO повторное нажатие на кнопку не работает, вероятно из-за прописанных стилей здесь
+  modalElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 300ms ease-in-out;
+    `;
+
+  const closeModal = event => {
+    const target = event.target;
+    if (target === modalElem || target.closest(btnClose) || event.code === 'Escape') {
+      modalElem.style.opacity = '0';
+    }
+
+    setTimeout(() => {
+    }, 300);
+
+    window.removeEventListener('keydown', closeModal)
+  }
+
+  const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = '1';
+    window.addEventListener('keydown', closeModal)
+  }
+
+  buttonElem.addEventListener('click', openModal);
+  modalElem.addEventListener('click', closeModal);
+}
+
+modalController({
+  modal: '.modal',
+  btnOpen: '.menu__order',
+  btnClose: '.modal__close'
+});
+
+const items = document.querySelectorAll('.questions__block');
+const buttons = document.querySelectorAll('.questions__button');
+const textWrapper = document.querySelectorAll('.questions__text-wrapper');
+
+const accordionController = (items, buttons, textWrapper) => {
+  console.log(items)
+  console.log(buttons)
+  console.log(textWrapper)
+
+  let heightWrapper = 0;
+  textWrapper.forEach((elem) => {
+    if (heightWrapper < elem.scrollHeight) {
+      heightWrapper = elem.scrollHeight;
+    }
+  })
+
+
+  buttons.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      for (let i = 0; i < items.length; i += 1) {
+        if (index === i) {
+          textWrapper[i].style.height =
+            items[i].classList.contains('questions__active') ?
+              '' : `${heightWrapper}px`;
+          items[i].classList.toggle('questions__active');
+        } else {
+          items[i].classList.remove('questions__active');
+          textWrapper[i].style.height = '';
+        }
+      }
+    });
+  });
+}
+
+accordionController(items,buttons,textWrapper);
+
+modalController({
+  modal: '.modal',
+  btnOpen: '.menu__order',
+  btnClose: '.modal__close'
+});
+
+
+
+
